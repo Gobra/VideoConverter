98fd064742fd1a4770dd1e0b6ad28d9aa6c2f097
Yaroslav Glushchenko <gobra.rus@gmail.com>
2020-12-11 15:16:56 +0100

    Initial

---
+++ converter.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import subprocess
+from argparse import ArgumentParser
+
+def main():
+    # args parser
+    parser = ArgumentParser()
+    parser.add_argument('-args', '--args', default='-c:v libx265 -c:a copy', help='ffmpeg arguments for conversion')
+    parser.add_argument('folder', help='folder to process files from')
+    args = parser.parse_args(sys.argv[1:])
+
+    # find files
+    exts = [".3g2", ".3gp", ".asf", ".asx", ".avi", ".flv", \
+        ".m2ts", ".mkv", ".mov", ".mp4", ".mpg", ".mpeg", \
+        ".rm", ".swf", ".vob", ".wmv"]
+    videoFiles = [os.path.join(args.folder, file) for file in os.listdir(args.folder) if file.endswith(tuple(exts))]
+
+    # process results
+    for video in videoFiles:
+        base = os.path.splitext(os.path.basename(video))[0]
+        output = os.path.join(args.folder, "Output", base + ".mp4")
+        command = "ffmpeg {} -i {} {}".format(args.args, video, output)
+        done = subprocess.run(command, capture_output=True, shell=True)
+        if 0 == done.returncode:
+            print("{} - OK".format(video))
+        else:
+            print("{} - FAILED".format(video))
+
+if __name__ == '__main__':
+    main()
